dbfb798d15df4d3b3171434a520fb17297cf681a
Jon Sledge <jonsledge39@gmail.com>
2020-09-08 17:10:47 -0400

    adds input from user and string interpolation

--- HelloWorldDemo/Program.cs
+++ HelloWorldDemo/Program.cs
@@ -7,6 +7,10 @@ namespace HelloWorldDemo
     static void Main(string[] args)
     {
       Console.WriteLine("Hello Jon!");
+      Console.WriteLine("What's your name?");
+      string input = Console.ReadLine();
+      Console.WriteLine($"Hello {input}");
+      Console.WriteLine("Hello {0}", input);
     }
   }
 }
