6456e3c94664d190c794eb33a9819d3f4056723e
Jon Sledge <jonsledge39@gmail.com>
2020-10-07 15:50:41 -0400

    added working todoapi

---
+++ todoapi/todorestapi/Models/TodoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todorestapi.Models;
+
+namespace todorestapi.Models
+{
+  public class TodoDbContext : DbContext
+  {
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<TodoItemDTO> TodoItems { get; set; }
+
+  }
+}
